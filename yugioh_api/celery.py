b49fe20d37dfd96dfd5bf4071f40f60fd34c8f6c
elMauro2003 <mauricioelanimal2003@gmail.com>
2025-01-05 23:13:40 -0500

    ref[celery]: celery refact

--- yugioh_api/celery.py
+++ yugioh_api/celery.py
@@ -10,7 +10,7 @@ app = Celery('yugioh_api')
 # Usar una cadena aquí significa que el worker no tendrá que serializar
 # el objeto de configuración para Child processes.
 # - namespace='CELERY' significa que todas las claves de configuración relacionadas con Celery
-#   deben tener un prefijo `CELERY_`.
+#   deben tener un prefijo `CELERY_`
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Cargar tareas de todos los módulos de aplicaciones registradas de Django
